ae10fbf56814655e2e82fff3e34334512420b020
Aditya Doshi <80141865+adidoshi@users.noreply.github.com>
2021-11-06 18:02:12 +0530

    docs : heroku file & readme update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-var express = require("express");
+const express = require("express");
 const app = require("../app");
-var router = express.Router();
+const router = express.Router();
 
 // Data
 const rooms = [
